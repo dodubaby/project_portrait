5977c7e07f91c14379b1cde7905110eb41da29dd
liangjinzhu <liangjinzhu001@ke.com>
2019-03-16 17:44:39 +0800

    feature：系统面板功能开发

--- ProjectPortraitConsole/src/function/dashboard/index.vue
+++ ProjectPortraitConsole/src/function/dashboard/index.vue
@@ -16,53 +16,35 @@
     </el-carousel>
 
     <el-row :gutter="10" style="margin-top: 30px">
-      <el-col :span="7">
-        <el-card shadow="always" header="项目概况 / 层级结构 / 引用关系">
-          <el-tag>项目全局</el-tag>
-          <el-tag>逻辑关系</el-tag>
+      <el-col>
+        <el-card shadow="always" header="崩溃预防">
+          <ul>
+            <li>检测到极可能被触发的<b>CRASH</b><a class="text danger">{{ badRuleAnalysis.errorCount }}</a>处</li>
+            <li>检测到代码逻辑<b>告警点</b><a class="text danger">{{ badRuleAnalysis.warnCount }}</a>处</li>
+          </ul>
         </el-card>
       </el-col>
-      <el-col :span="7">
-        <el-card shadow="always" header="功能管理 / 人效管理 / 质量把控与管理">
-          <el-tag>标签</el-tag>
-          <el-tag>优化/质量筛查</el-tag>
+      <el-col>
+        <el-card shadow="always" header="优化/质量筛查">
+          <li>检测到重复定义了<a class="text warning">{{qualityaAnalysis.stringReplicationCount}}</a>个<b>字符串资源</b></li>
+          <li>检测到重复定义了<a class="text warning">{{qualityaAnalysis.colorReplicationCount}}</a>个<b>颜色值资源</b></li>
+          <li>检测到共有<a class="text warning">{{qualityaAnalysis.fileLongCount}}</a>个文件<b>行数超过500行</b>，需要重构</li>
+          <li>检测到共有<a class="text warning">{{qualityaAnalysis.fileLongCount}}</a>个文件<b>体积较大</b>，需要重构</li>
+          <li>检测到代码和资源中共有<a class="text warning">{{badRuleAnalysis.hardcodeCount}}</a>处<b>硬编码</b>，潜在风险</li>
         </el-card>
       </el-col>
-      <el-col :span="5">
-        <el-card shadow="always" header="开发工具">
-          <el-tag>规则无限可能</el-tag>
+      <el-col>
+        <el-card shadow="always" header="功能管理、人效管理">
+          <li>目前共有<a class="text info">{{functionAnalysis.functionNumbers}}</a>位<b>参与及贡献者</b></li>
+          <li>累计完成<a class="text success">{{functionAnalysis.coreFunctionCounts}}</a>个<b>核心业务功能点</b></li>
+          <li>设计并实现了<a class="text success">{{functionAnalysis.commonFunctionCounts}}</a>个<b>通用能力模块</b></li>
         </el-card>
       </el-col>
-      <el-col :span="5">
-        <el-card shadow="always" header="系统">
-          <el-tag>控制台</el-tag>
-          <el-tag>关于</el-tag>
-        </el-card>
-      </el-col>
-      <el-col :span="5">
-        <el-card shadow="always" header="Crash预防">
-          <el-tag>可能Crash点{{ badRuleAnalysis.errorCount }}个</el-tag>
-          <el-tag>告警点{{ badRuleAnalysis.warnCount }}个</el-tag>
-        </el-card>
-      </el-col>
-      <el-col :span="5">
-        <el-card shadow="always" header="风险范围检索">
-          <el-tag>支持检索内容变化的风险范围</el-tag>
-        </el-card>
-      </el-col>
-      <el-col :span="5">
-        <el-card shadow="always" header="质量分析">
-          <el-tag>重复定义的字符串资源{{qualityaAnalysis.stringReplicationCount}}个</el-tag>
-          <el-tag>重复定义的颜色值资源{{qualityaAnalysis.colorReplicationCount}}</el-tag>
-          <el-tag>文件行数过长的{{qualityaAnalysis.fileLongCount}}个</el-tag>
-          <el-tag>硬编码{{badRuleAnalysis.hardcodeCount}}个</el-tag>
-        </el-card>
-      </el-col>
-      <el-col :span="5">
-        <el-card shadow="always" header="功能人员统计">
-          <el-tag>共有{{functionAnalysis.functionNumbers}}人参与此项目</el-tag>
-          <el-tag>包含{{functionAnalysis.coreFunctionCounts}}个核心功能</el-tag>
-          <el-tag>提供通用能力包{{functionAnalysis.commonFunctionCounts}}个</el-tag>
+      <el-col>
+        <el-card shadow="always" header="增强工具">
+          <li>如果你想<b>评估风险范围</b>亦或<b>查询引用关系</b>，欢迎进行<a class="text success" style="font-size: 18px">逻辑关系查询</a></li>
+          <li>如果你要检查<b>代码层级结构</b>情况如何，可以尝试<a class="text success" style="font-size: 18px">层级关系查询</a></li>
+          <li>如果你有需要，进行IDE无法支持的<b>多维度检索</b>，猛敲这里<a class="text success" style="font-size: 18px">多维度检索</a></li>
         </el-card>
       </el-col>
     </el-row>
@@ -102,13 +84,30 @@ export default {
 <style>
   .el-carousel__item h1 {
     color: #475669;
-    font-size: 30px;
+    font-size: 25px;
     align-content: center;
     align-items: center;
     align-self: center;
     opacity: 0.75;
     line-height: 120px;
     margin: 20px;
+  }
+
+  .el-col {
+    width: 48%;
+    margin: 1%;
+  }
+
+  .el-col li {
+    font-size: 16px;
+    line-height: 33px;
+    color: #606266;
+  }
+
+  .el-col li b {
+    font-size: 16px;
+    color: #303133;
+    margin: 5px;
   }
 
   .el-carousel__item:nth-child(2n) {
@@ -118,4 +117,27 @@ export default {
   .el-carousel__item:nth-child(2n+1) {
     background-color: #d3dce6;
   }
+
+  .text {
+    margin: 5px;
+    font-weight: bold;
+    font-size: 35px;
+  }
+
+  .text.success {
+    color: #67C23A;
+  }
+
+  .text.warning {
+    color: #E6A23C;
+  }
+
+  .text.danger {
+    color: #F56C6C;
+  }
+
+  .text.info {
+    color: #303133;
+  }
+
 </style>
